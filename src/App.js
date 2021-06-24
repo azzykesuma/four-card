f5da800afd74f61716462abd69833802721d8a07
Azzy Kesuma <official.azzy@gmail.com>
2021-06-24 18:38:18 +0700

    adding all files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import imagesSpv from './images/icon-supervisor.svg';
+import imagesTb from './images/icon-team-builder.svg';
+import imagesKarma from './images/icon-karma.svg';
+import imagesCalc from './images/icon-calculator.svg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div class='MainContainer'>
+      <div className='header'>
+        <h1>Reliable, efficient delivery <span>Powered by Technology</span></h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Our Artificial Intelligence powered tools use millions of project data points 
+          to ensure that your project is successful
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      </div>
+
+      <div className='cardContainer'>
+        <div className='card a'>
+          <div className='cardInterior'>
+            <h1>Supervisor</h1>
+            <p>Monitors activity to identify project roadblocks</p>
+            <img src={imagesSpv} alt='magnifier' />
+          </div>
+        </div>
+        <div className='card b'>
+          <div className='cardInterior'>
+            <h1>Team Builder</h1>
+            <p>Scans our talent network to create the optimal team for your project</p>
+            <img src={imagesTb} alt='magnifier' />
+          </div>
+        </div>
+        <div className='card c'>
+          <div className='cardInterior'>
+            <h1>Karma</h1>
+            <p>Regularly evaluates our talent to ensure quality</p>
+            <img src={imagesKarma} alt='magnifier' />
+          </div>
+        </div>
+        <div className='card d'>
+          <div className='cardInterior'>
+            <h1>Calculator</h1>
+            <p>Uses data from past projects to provide better delivery estimatess</p>
+            <img src={imagesCalc} alt='magnifier' />
+          </div>
+        </div>
+      </div>
+      <div>
+        <p class="attribution">
+          Challenge by <a href="https://www.frontendmentor.io?ref=challenge" rel='noreferrer' target="_blank">Frontend Mentor</a>. 
+          Coded by <a href="https://www.frontendmentor.io/profile/azzykesuma">Azzy</a>.
+        </p>
+      </div>
+  </div>
   );
 }
 
